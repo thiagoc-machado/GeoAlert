770df9d5d45bcfd6214f1306cf28f0fb349eb007
thiagoc-machado <thiagocmachado@yahoo.com.br>
2025-06-07 13:48:37 +0200

    scheleton

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Welcome to GeoAlert</h1>
+    <p>This is the initial view</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
